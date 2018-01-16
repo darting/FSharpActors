ea2b79e027117122b1ba57b4be2d63e2b03cdf33
Darting <guo.yong@mnltechnology.com>
2018-01-16 18:18:22 +0800

    init

---
+++ Actors.fs
@@ -0,0 +1,141 @@
+namespace FSharp
+
+
+module Actors =
+
+    open System
+    open System.Threading
+    open Microsoft.Extensions.Logging
+
+    type ActorID = ActorID of string
+
+    type ActorHostConfiguration = {
+        Logger : ILogger seq
+        CancellationToken : CancellationToken
+        Name : string
+    }
+    with 
+        static member Create (?name, ?loggers, ?cancellationToken) =
+            let proc = Diagnostics.Process.GetCurrentProcess()
+            let processName, processID = proc.ProcessName, proc.Id
+            let defaultHostName = sprintf "%s_%s_%d" Environment.MachineName processName processID
+            let hostName = defaultArg name defaultHostName
+            let ct = defaultArg cancellationToken Async.DefaultCancellationToken
+            { 
+              Name = hostName
+              CancellationToken = ct 
+              Logger = (defaultArg loggers Seq.empty)
+            }
+
+    type StateStore<'State> = {
+        Read : unit -> Async<'State>
+        Write : 'State -> Async<unit>
+    }
+
+    type Reducer<'State, 'Action> = 'State -> 'Action -> 'State
+
+    type ActorConfiguration<'State, 'Action> = {
+        Store : StateStore<'State>
+        Reducer : Reducer<'State, 'Action>
+        TimeOutInMills : int option
+    }
+
+    type Agent<'T> private (actorID : ActorID, processor : MailboxProcessor<'T>, cts : CancellationTokenSource) =
+        static member Start actorID job =
+            let cts = new CancellationTokenSource ()
+            let proc = new MailboxProcessor<'T> (job, cts.Token)
+            do
+                proc.Error.Add (fun ex -> Console.WriteLine ex.Message)
+                proc.Start ()
+            new Agent<'T>(actorID, proc, cts)
+
+        member __.PostAndAsyncReply (buildMessage, ?timeout) = processor.PostAndAsyncReply (buildMessage, ?timeout=timeout)
+        member __.Ask (buildMessage, ?timeout) = processor.PostAndTryAsyncReply (buildMessage, ?timeout=timeout)
+        member __.Post message = processor.Post message
+        member __.ID = actorID
+        interface IDisposable with
+            member __.Dispose () =
+                (processor :> IDisposable).Dispose ()
+                cts.Cancel ()
+
+
+
+    module ActorHost = 
+
+        type ActorMessage<'State, 'Action> = 'Action * AsyncReplyChannel<'State>
+
+        type ActorHostState<'T> = Map<string, Agent<'T>>
+
+        type ActorHostAction =
+            | Get of ActorID
+            | Remove of ActorID
+
+        type private ActorHostMessage<'State, 'Action> = ActorHostAction * AsyncReplyChannel<Agent<ActorMessage<'State, 'Action>>> option
+
+        type IActorHost<'T> =
+            abstract GetActor : ActorID -> Async<Agent<'T>>
+            abstract RemoveActor : ActorID -> unit
+
+        let private workerCreator<'State, 'Action>
+                (configuration : ActorConfiguration<'State, 'Action>)
+                (actorHost : MailboxProcessor<ActorHostMessage<'State, 'Action>>)
+                (actorID : ActorID) =
+            let timeOutInMills = match configuration.TimeOutInMills with
+                                 | Some x -> x
+                                 | None -> -1
+            let store = configuration.Store
+            let reducer = configuration.Reducer
+            let worker = Agent<'Action * AsyncReplyChannel<'State>>.Start actorID
+                                (fun inbox -> 
+                                    let rec loop previousState = async {
+                                        try
+                                            let! action, channel = inbox.Receive timeOutInMills
+                                            let newState = reducer previousState action
+                                            channel.Reply newState
+                                            return! loop newState
+                                        with
+                                        // | :? TimeoutException -> 
+                                        //     actorHost.RemoveActor actorID
+                                        | ex -> 
+                                            try do! store.Write previousState
+                                            with _ -> ()
+                                            actorHost.Post (Remove actorID, None)
+                                    }
+                                    async {
+                                        let! state = store.Read ()
+                                        return! loop state
+                                    })
+            
+            worker
+        
+        let create<'State, 'Action> (actorConfiguration : ActorConfiguration<'State, 'Action>) =
+            let configuration = ActorHostConfiguration.Create ()
+            let actorHost = 
+                new MailboxProcessor<ActorHostMessage<'State, 'Action>> ((fun inbox -> 
+                    let rec loop (state : ActorHostState<ActorMessage<'State, 'Action>>) = async {
+                        let! message, replyChannel = inbox.Receive()
+                        let newState = match message with
+                                        | Get (ActorID actorID) ->
+                                            let state', actor = 
+                                                match Map.tryFind actorID state with
+                                                | Some actor -> state, actor
+                                                | None -> 
+                                                    let actor = workerCreator actorConfiguration inbox (ActorID actorID)
+                                                    Map.add actorID actor state, actor
+                                            replyChannel |> Option.iter (fun ch -> ch.Reply actor)
+                                            state'
+                                        | Remove (ActorID actorID) ->
+                                            match Map.tryFind actorID state with
+                                            | Some actor -> (actor :> IDisposable).Dispose()
+                                            | None -> ()
+                                            Map.remove actorID state
+                        return! loop newState
+                    }
+                    loop Map.empty), configuration.CancellationToken)
+            actorHost.Start ()
+            { new IActorHost<ActorMessage<'State, 'Action>> with
+                member __.GetActor actorID = actorHost.PostAndAsyncReply (fun ch -> Get actorID, Some ch)
+                member __.RemoveActor actorID = actorHost.Post (Remove actorID, None) }
+
+
+
---
+++ Program.fs
@@ -0,0 +1,80 @@
+﻿open System
+open FSharp.Actors
+open FSharp.Actors.ActorHost
+
+
+type GameStateForList = string list
+type GameStateForInt = int
+
+type Command =
+    | Increase
+    | Decrease
+
+type Event<'State> =
+    | Increased of 'State
+    | Decreased of 'State
+
+let listReducer state cmd =
+    match cmd, state with
+    | Increase, _ -> "+" :: state
+    | Decrease, [] -> []
+    | Decrease, _ :: t -> t
+
+let intReducer state cmd =
+    match cmd with
+    | Increase -> state + 1
+    | Decrease -> state - 1
+
+let listStore : StateStore<GameStateForList> = 
+    { Read = fun () -> async.Return []
+      Write = fun _ -> async.Zero () }
+
+let intStore : StateStore<GameStateForInt> = 
+    { Read = fun () -> async.Return 0
+      Write = fun _ -> async.Zero () }
+
+let listCfg = {
+    Store = listStore
+    Reducer = listReducer
+    TimeOutInMills = None
+}
+
+let intCfg = {
+    Store = intStore
+    Reducer = intReducer
+    TimeOutInMills = Some 2_00
+}
+
+
+[<EntryPoint>]
+let main argv =
+
+    printfn "Hello World from F#!"
+
+    let timeOutInMills = 2_000
+
+    let host1 = ActorHost.create listCfg
+    let host2 = ActorHost.create intCfg
+
+    let worker (actorHost : IActorHost<ActorMessage<'State, Command>>) = 
+        async {
+            let actorID = ActorID "game1"
+
+            use! actor = actorHost.GetActor actorID
+            let! rsp1 = actor.PostAndAsyncReply (fun ch -> Increase, ch)
+            System.Console.WriteLine ("1> {0}", rsp1)
+            let! rsp2 = actor.PostAndAsyncReply (fun ch -> Increase, ch)
+            System.Console.WriteLine ("2> {0}", rsp2)
+            do! Async.Sleep 3_000
+            
+            use! actor = actorHost.GetActor actorID
+            let! rsp3 = actor.PostAndAsyncReply ((fun ch -> Increase, ch), timeOutInMills)
+            System.Console.WriteLine ("3> {0}", rsp3)
+            let! rsp4 = actor.PostAndAsyncReply (fun ch -> Decrease, ch)
+            System.Console.WriteLine ("4> {0}", rsp4)
+        }
+
+    host1 |> worker |> Async.RunSynchronously
+    host2 |> worker |> Async.RunSynchronously
+
+    0
