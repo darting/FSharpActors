b5883eecdd8ec0f9ca6b234f263337c02ed75809
Valor <valorzhong@gmail.com>
2018-02-27 22:36:45 +0800

    ...

--- ActorRuntime.fs
+++ ActorRuntime.fs
@@ -1,5 +1,6 @@
 module FSharpActors.ActorRuntime
 
+open System
 open System.Threading
 open System.Collections.Concurrent
 open FSharpActors.Types
@@ -11,33 +12,33 @@ type Actor<'T> private (actorID : ActorID, mailbox : MailboxProcessor<'T>, cts :
     static member Start<'T> actorID job =
         let cts = new CancellationTokenSource ()
         let mailbox = MailboxProcessor<'T>.Start (job, cts.Token)
-        new Actor<'T>(actorID, mailbox, cts)
+        new Actor<'T>(actorID, mailbox, cts) :> IActor<'T>
 
-    static member (<?) (actor : IActor<'T>) buildMessage = actor.Ask buildMessage
+    static member (<?) (actor : IActor<'T>, buildMessage) = actor.Ask buildMessage
 
     interface IActor<'T> with
         member __.ID = actorID
         member __.Ask (buildMessage, ?timeout) = mailbox.PostAndTryAsyncReply (buildMessage, defaultArg timeout defaultTimeOutInMills)
         member __.Tell message = mailbox.Post message
         member __.Dispose () =
-            // (mailbox :> IDisposable).Dispose ()
+            (mailbox :> IDisposable).Dispose ()
             cts.Cancel ()
 
 type ActorRuntime () =
 
-    let registry = new ConcurrentDictionary<ActorID, IActor<_>>()
+    let registry = new ConcurrentDictionary<ActorID, obj>()
 
     interface IActorRuntime with
-
-    member this.Spawn<'T> (actorID : ActorID) job = 
-        async {
-            match registry.TryGetValue actorID with
-            | true, x -> return x :> IActor<'T>
-            | false, _ -> 
-                let actor = Actor<'T>.Start actorID job
-                registry.TryAdd (actorID, actor)
-                return actor :> IActor<'T>
-        }
+        member __.Spawn (actorID : ActorID) (job : Job<'T>) = 
+            async {
+                match registry.TryGetValue actorID with
+                | true, x -> return x :?> IActor<'T>
+                | false, _ -> 
+                    let actor = Actor<'T>.Start actorID job
+                    if not (registry.TryAdd (actorID, actor)) then
+                        actor.Dispose()
+                    return actor
+            }
         
 
 
