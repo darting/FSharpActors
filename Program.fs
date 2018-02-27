5ed017bc62dcba6f3342cde149c29acc07adb0bd
Valor <valorzhong@gmail.com>
2018-02-28 00:06:38 +0800

    consul

--- Program.fs
+++ Program.fs
@@ -1,27 +1,46 @@
 open FSharpActors
 open FSharpActors.ActorHost
 open System
+open Consul
+
 
 type Message = 
     | Ping
+    | Connect 
+
+let connect () =
+    async {
+        let id = "node-1"
+        let name = "node-1-name"
+        use client = new ConsulClient (fun x -> x.Address <- Uri "http://localhost:32769")
+        let register = AgentServiceRegistration(ID = id, Name = name, Tags = [||])
+        let! _ = client.Agent.ServiceRegister register |> Async.AwaitTask
+        return ()
+    }
 
 [<EntryPoint>]
 let main _ = 
 
-    let actorHost = ActorHost.Start ()
+    use actorHost = ActorHost.Start ()
 
-    let actor = actorHost.Spawn 1 (fun (inbox : MailboxProcessor<Message>) -> 
+    let actor = actorHost.Spawn 1 (fun inbox -> 
                     let rec loop () = async {
                         let! msg = inbox.Receive()
-                        printfn "received : %A" msg
+                        match msg with 
+                        | Connect ->
+                            do! connect()
+                        | Ping ->
+                            printfn "received : %A" msg
                         return! loop ()
                     }
                     loop ()) 
                     |> Async.RunSynchronously
 
     actor.Tell Ping
+    actor.Tell Connect
     actor.Tell Ping
     actor.Tell Ping
 
     Console.Read () |> ignore
+    
     0
