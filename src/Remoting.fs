d1b7c12468392904abe5bd2be66b6bd26d628d03
Valor <valorzhong@gmail.com>
2018-03-03 01:25:41 +0800

    update

--- src/Remoting.fs
+++ src/Remoting.fs
@@ -1,8 +1,24 @@
-module FSharpActors.Remotings
+module FSharpActors.Remoting
 
+open System.Threading
+open System.Threading.Tasks
+open Grpc.Core
+open FSharp.Control.Tasks.ContextInsensitive
 open FSharpActors.Remoting
 
-type RemotingImpl () = 
-    inherit FSharpActors.Remoting.ActorRuntimeBase ()
+
+type Remoting () = 
+    inherit ActorRuntime.ActorRuntimeBase ()
+
+    override this.Receive(requestStream : IAsyncStreamReader<MessageBatch>
+                         , responseStream : IServerStreamWriter<Unit>
+                         , context : ServerCallContext) = 
+
+        task {
+            let cts = new CancellationTokenSource()                         
+            let! x = requestStream.MoveNext cts.Token
+            ()
+        } 
+
 
     
